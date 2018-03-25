f9fb4b8ecb3c27f9fea8b4c150b4b50bfd9f93d1
ramror <ramesh.ramror@gmail.com>
2018-03-25 18:18:58 +0530

    Add Devise

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,23 @@ class ItemsController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @item.update(item_params)
+			redirect_to item_path(@item)
+		else
+			render "edit"
+		end
+	end
+
+	def destory
+		@item.destory
+		redirect_to root_path
+	end
+					
+
 	private
 	def item_params
 		params.require(:item).permit(:title, :description)
